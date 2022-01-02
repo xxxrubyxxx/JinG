437fbd4283dbad2aca8d9e6166ad1c24845767ad
ruby <qiangjian71@gmail.com>
2022-01-03 00:48:26 +0800

    JinG

---
+++ src/main/java/CustomHttpServerHandler.java
@@ -0,0 +1,62 @@
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.*;
+import io.netty.handler.codec.json.JsonObjectDecoder;
+import io.netty.util.AsciiString;
+import jdk.nashorn.internal.parser.JSONParser;
+import lombok.extern.slf4j.Slf4j;
+
+import java.nio.charset.StandardCharsets;
+
+@Slf4j
+public class CustomHttpServerHandler extends SimpleChannelInboundHandler<FullHttpRequest> {
+    private static final AsciiString CONNECTION = AsciiString.cached("Connection");
+    private static final AsciiString KEEP_ALIVE = AsciiString.cached("keep-alive");
+    private static final AsciiString CONTENT_TYPE = AsciiString.cached("Content-Type");
+    private static final AsciiString CONTENT_LENGTH = AsciiString.cached("Content-Length");
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, FullHttpRequest fullHttpRequest) {
+        log.info("Handle http request:{}", fullHttpRequest);
+        String uri = fullHttpRequest.uri();
+        log.info(uri);
+
+        FullHttpResponse response;
+        try {
+            String responseHtml = "<html><body>" + "hello" + "</body></html>";
+            byte[] responseBytes = responseHtml.getBytes(StandardCharsets.UTF_8);
+            response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK,
+                    Unpooled.wrappedBuffer(responseBytes));
+            response.headers().set(CONTENT_TYPE, "text/html; charset=utf-8");
+            response.headers().setInt(CONTENT_LENGTH, response.content().readableBytes());
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+            String responseHtml = "<html><body>" + e.toString() + "</body></html>";
+            byte[] responseBytes = responseHtml.getBytes(StandardCharsets.UTF_8);
+            response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.INTERNAL_SERVER_ERROR,
+                    Unpooled.wrappedBuffer(responseBytes));
+            response.headers().set(CONTENT_TYPE, "text/html; charset=utf-8");
+        }
+        boolean keepAlive = HttpUtil.isKeepAlive(fullHttpRequest);
+        if (!keepAlive) {
+            ctx.write(response).addListener(ChannelFutureListener.CLOSE);
+        } else {
+            response.headers().set(CONNECTION, KEEP_ALIVE);
+            ctx.write(response);
+        }
+    }
+
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) {
+        ctx.flush();
+    }
+}
